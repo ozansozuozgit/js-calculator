ac43ef69dfceac57c4585c80662196453789bb51
Ozan Sozuoz <ozansozuoz@hotmail.com>
2020-04-13 16:45:32 +0300

    Code redesign. Changed all event listeners to more suitable targets

--- scripts/app.js
+++ scripts/app.js
@@ -1,95 +1,97 @@
-const inputs = document.querySelectorAll(".input");
 const topResult = document.querySelector("#top-result");
 const bottomResult = document.querySelector("#bottom-result");
 const container = document.querySelector("#container");
 const operators = document.querySelectorAll(".operators");
+const numbers = document.querySelectorAll(".numbers");
+const equals = document.querySelector("#equal");
+const btnAC = document.querySelector("#btn-ac");
+const btnBack = document.querySelector("#btn-back");
+const dot = document.querySelector("#dot");
+const buttons = document.querySelectorAll(".buttons");
 topResult.fontSize = "60px";
-// bottomResult.fontSize = "12px";
 let size = topResult.fontSize.replace("px", "");
 let dotUsed = false;
 let equalsUsed = false;
 
-inputs.forEach((input) => {
-  input.addEventListener("click", (e) => {
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const audio = document.querySelector("#keypressSound");
+    // audio.currenTime = 0;
+    audio.volume = 0.1;
+    audio.load();
+    audio.play();
+  });
+});
+
+numbers.forEach((number) => {
+  number.addEventListener("click", (e) => {
     if (equalsUsed === true) {
+      equalsReset();
       topResult.textContent = "";
-      topResult.style.fontSize = "60px";
-      size = topResult.fontSize.replace("px", "");
-      bottomResult.style.alignSelf = "end";
-      bottomResult.style.fontSize = "40px";
-      equalsUsed = false;
     }
-    // Only 25 characters allowed
     if (topResult.textContent.length < 25) {
       // Decrease font size as we get to the border of the screen
       if (topResult.clientWidth + 10 > container.clientWidth && size >= 20) {
-        size -= 5;
-        topResult.style.fontSize = size.toString() + "px";
+        // size -= 5;
+        // topResult.style.fontSize = size.toString() + "px";
       }
       topResult.textContent += e.target.innerText;
     }
     operate();
   });
 });
+equals.addEventListener("click", () => {
+  if (equalsUsed === false && bottomResult.textContent !== "") {
+    topResult.classList.add("scale-down");
+    bottomResult.classList.add("scale-up");
+    equalsUsed = true;
+  }
+});
+
+btnBack.addEventListener("click", () => {
+  // Avoid undefined when there is an empty string
+  const lastChar =
+    topResult.textContent !== ""
+      ? topResult.textContent[topResult.textContent.length - 1]
+      : "";
+
+  topResult.textContent = topResult.textContent.slice(0, -1);
+  // Create a new span element
+  topResult.insertAdjacentHTML(
+    "beforeend",
+    `<span class="numbers vanishIn">${lastChar}</span>`
+  );
+  setTimeout(() => {
+    backSpace();
+  }, 350);
+});
+
+btnAC.addEventListener("click", () => {
+  topResult.classList.add("vanishOut");
+  bottomResult.classList.add("vanishOut");
+  setTimeout(() => {
+    topResult.textContent = "";
+    bottomResult.textContent = "";
+    topResult.classList.remove("vanishOut");
+    bottomResult.classList.remove("vanishOut");
+  }, 900);
+});
 
 operators.forEach((operator) => {
   operator.addEventListener("click", (e) => {
-    const operatorChar = e.target.innerText;
     dotUsed = false;
-
-    if (equalsUsed === true) {
-      topResult.textContent = bottomResult.textContent;
-      bottomResult.textContent = "";
-      topResult.style.fontSize = "60px";
-      bottomResult.style.alignSelf = "end";
-      bottomResult.style.fontSize = "40px";
-      equalsUsed = false;
-    }
-
-    // CONVERT TO SWITCH CASE
-    if (operatorChar === "+") {
-      operatorConcat(operatorChar);
-    }
-    if (operatorChar === "-") {
-      operatorConcat(operatorChar);
-    }
-    if (operatorChar === "×") {
-      operatorConcat(operatorChar);
-    }
-    if (operatorChar === "÷") {
-      operatorConcat(operatorChar);
-    }
-    if (operatorChar === "%") {
-      operatorConcat(operatorChar);
-    }
-    if (operatorChar === "=" && !(bottomResult.textContent === "")) {
-      bottomResult.style.fontSize = "60px";
-      bottomResult.style.alignSelf = "baseline";
-      topResult.style.fontSize = "40px";
-      equalsUsed = true;
-      // bottomResult.classList.add("scale-up");
-    }
-    if (operatorChar === "." && dotUsed === false) {
-      operatorConcat(operatorChar);
-      dotUsed = true;
-    }
-    if (operatorChar === "«") {
-      topResult.textContent = topResult.textContent.slice(0, -1);
-      if (topResult.textContent === "") {
-        bottomResult.textContent = "";
-      }
-      // Size it back to original if width is becoming smaller than overflow of container
-      if (topResult.clientWidth + 10 < container.clientWidth && size < 60) {
-        size += 5;
-        topResult.style.fontSize = size.toString() + "px";
-      }
-      operate();
-    }
-    if (operatorChar === "AC") {
-      topResult.textContent = "";
-      bottomResult.textContent = "";
-    }
+    operatorConcat(e.target.innerText);
   });
+});
+
+dot.addEventListener("click", (e) => {
+  if (
+    dotUsed === false &&
+    !isNaN(topResult.textContent[topResult.textContent.length - 1])
+  ) {
+    topResult.textContent += e.target.innerText;
+    dotUsed = true;
+  }
 });
 
 // For disallowing multiple operators to be added concurrently
@@ -103,17 +105,51 @@ function operatorConcat(operator) {
   } else {
     topResult.textContent += operator;
   }
-}
-function operate() {
-  console.log(topResult.textContent);
 
+  if (equalsUsed === true) {
+    topResult.textContent = bottomResult.textContent;
+    bottomResult.textContent = "";
+    equalsReset();
+    topResult.textContent += operator;
+  }
+}
+
+function operate() {
   const expression = topResult.textContent
     .replace(/÷/g, "/")
     .replace(/×/g, "*");
   // For backspace errors, since we don't want to evaluate with an operation at the end
   if (!isNaN(topResult.textContent[topResult.textContent.length - 1])) {
     const result = math.evaluate(expression);
-    bottomResult.textContent = math.round(result, 3);
+
+    // Keep the bottom result hidden when there is no operator included in the topResult
+    if (!isNaN(topResult.textContent)) {
+      bottomResult.textContent = "";
+    } else {
+      bottomResult.textContent = math.round(result, 3);
+    }
   }
-  console.log(expression);
+}
+
+function backSpace() {
+  topResult.textContent = topResult.textContent.slice(0, -1);
+
+  if (topResult.clientWidth + 10 < container.clientWidth && size < 60) {
+    size += 5;
+    topResult.style.fontSize = size.toString() + "px";
+  }
+  if (equalsUsed === true) {
+    operate();
+    bottomResult.textContent = "";
+    equalsReset();
+  }
+  operate();
+}
+
+function equalsReset() {
+  if (equalsUsed === true) {
+    topResult.classList.remove("scale-down");
+    bottomResult.classList.remove("scale-up");
+    equalsUsed = false;
+  }
 }
